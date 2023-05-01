1c461504f0048bdb980ca225bae0cad83be3ad96
YASH MITTAL <mittal.yash12@hotmail.com>
2023-05-01 23:43:34 +0530

    Fix: Headers
    Signed-off-by: YASH MITTAL <mittal.yash12@hotmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yash Mittal';
+export const SITE_DESCRIPTION = 'Hey, there welcome to my Portfolio.';
