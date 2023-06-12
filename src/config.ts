259f09e2dfbe5b24a0aaa6d8a8962f93f53ce65e
Yash Mittal <mittal.yash12@hotmail.com>
2023-06-12 22:51:58 +0530

    fix: changes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "techsavvyash's journey.";
+export const SITE_DESCRIPTION = "Yash Mittal's Portfolio!";
